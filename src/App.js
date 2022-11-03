431d3de68648ebb96ee82560f54e2e92355d4a39
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2022-11-03 11:55:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Airbnb from "./components/Airbnb";
+
+function App() {
+  return (
+    <div className="mainContainer">
+      <Airbnb />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Airbnb.js
@@ -0,0 +1,11 @@
+const Airbnb = () => {
+    return(
+        <div className="container">
+            <h1>The Evolution of Airbnb's Frontend</h1>
+            <p>@spikebrehm</p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCwpB80M_RF88cSstwNefwMMffO6VeKN5cqQ&usqp=CAU" alt="logo" />
+        </div>
+    )
+}
+
+export default Airbnb;
